964c6d708290223357bb4e3fe03eac4bdfe4eea6
MD SOHAG MIYA <83050584+dev-mdsohagmiya@users.noreply.github.com>
2025-09-08 10:40:01 +0600

    updated title

--- components/ui/Title.jsx
+++ components/ui/Title.jsx
@@ -1,6 +1,6 @@
 export const Title = ({ text }) => {
   return (
-    <h2 className="text-[24px] md:text-[32px] lg:text-[42px] font-[700] text-[#1A1A1A]">
+    <h2 className="text-[24px] md:text-[32px] lg:text-[42px] font-[500] text-[#1A1A1A]">
       {" "}
       {text}
     </h2>
